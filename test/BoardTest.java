e4667c42dda96810bda1c255d2e4ac6172a879a8
Krista Gurney <gurneykri@gmail.com>
2018-11-05 20:34:39 -0700

    adjusting validateCheckRow to follow the same pattern as checkCol

--- test/BoardTest.java
+++ test/BoardTest.java
@@ -66,52 +66,94 @@ public class BoardTest{
         Board board = new Board();
         try {
             board.loadBoard(path);
-            board.checkRow('2', 0);
-            assertEquals(true, true);
+            assertEquals(board.checkRow('2', 0), true);
+            assertEquals(board.checkRow('3', 0), true);
+            assertEquals(board.checkRow('1', 0), true);
+
+            assertEquals(board.checkRow('1', 1), true);
+            assertEquals(board.checkRow('3', 1), true);
+            assertEquals(board.checkRow('4', 1), true);
+
+            assertEquals(board.checkRow('3', 2), true);
+            assertEquals(board.checkRow('1', 2), true);
+            assertEquals(board.checkRow('4', 2), true);
+
+            assertEquals(board.checkRow('2', 3), true);
+            assertEquals(board.checkRow('1', 3), true);
+            assertEquals(board.checkRow('3', 3), true);
+
         }catch (IOException e){
             e.printStackTrace();
         }catch (InvalidBoardException e){
             assertEquals(true, false);
         }
     }
-    
-    @Test
-    public void validateCheckBlock(){
-        Board board = new Board();
-        try {
-            board.loadBoard(path);
-            /*
-                2 - 3 1
-                1 3 - 4
-                3 1 4 -
-                - 2 1 3*/
-            board.checkBlock('2',0,0);
-            board.checkBlock('-', 0, 1 );
-            board.checkBlock('3', 0, 2 );
-            board.checkBlock('1', 0, 3 );
 
-            board.checkBlock('1', 1, 0 );
-            board.checkBlock('3', 1, 1 );
-            board.checkBlock('-', 1, 2 );
-            board.checkBlock('4', 1, 3 );
+//    @Test
+//    public void validateCheckCol(){
+//        Board board = new Board();
+//        try {
+//            board.loadBoard(path);
+//            assertEquals(board.checkCol('2', 0), true);
+//            assertEquals(board.checkCol('1', 0), true);
+//            assertEquals(board.checkCol('3', 0), true);
+//
+//            assertEquals(board.checkCol('3', 1), true);
+//            assertEquals(board.checkCol('1', 1), true);
+//            assertEquals(board.checkCol('2', 1), true);
+//
+//            assertEquals(board.checkCol('3', 2), true);
+//            assertEquals(board.checkCol('4', 2), true);
+//            assertEquals(board.checkCol('1', 2), true);
+//
+//            assertEquals(board.checkCol('1', 3), true);
+//            assertEquals(board.checkCol('4', 3), true);
+//            assertEquals(board.checkCol('3', 3), true);
+//
+//        }catch (IOException e){
+//            e.printStackTrace();
+//        }catch (InvalidBoardException e){
+//            assertEquals(true, false);
+//        }
+//    }
 
-            board.checkBlock('3', 2, 0 );
-            board.checkBlock('1', 2, 1 );
-            board.checkBlock('4', 2, 2 );
-            board.checkBlock('-', 2, 3 );
-
-            board.checkBlock('-', 3, 0 );
-            board.checkBlock('2', 3, 1 );
-            board.checkBlock('1', 3, 2 );
-            board.checkBlock('3', 3, 3 );
-
-            assertEquals(true, true);
-        }catch (IOException e){
-            e.printStackTrace();
-        }catch (InvalidBoardException e){
-            assertEquals(true, false);
-        }
-    }
+//    @Test
+//    public void validateCheckBlock(){
+//        Board board = new Board();
+//        try {
+//            board.loadBoard(path);
+//            /*
+//                2 - 3 1
+//                1 3 - 4
+//                3 1 4 -
+//                - 2 1 3*/
+//            board.checkBlock('2',0,0);
+//            board.checkBlock('-', 0, 1 );
+//            board.checkBlock('3', 0, 2 );
+//            board.checkBlock('1', 0, 3 );
+//
+//            board.checkBlock('1', 1, 0 );
+//            board.checkBlock('3', 1, 1 );
+//            board.checkBlock('-', 1, 2 );
+//            board.checkBlock('4', 1, 3 );
+//
+//            board.checkBlock('3', 2, 0 );
+//            board.checkBlock('1', 2, 1 );
+//            board.checkBlock('4', 2, 2 );
+//            board.checkBlock('-', 2, 3 );
+//
+//            board.checkBlock('-', 3, 0 );
+//            board.checkBlock('2', 3, 1 );
+//            board.checkBlock('1', 3, 2 );
+//            board.checkBlock('3', 3, 3 );
+//
+//            assertEquals(true, true);
+//        }catch (IOException e){
+//            e.printStackTrace();
+//        }catch (InvalidBoardException e){
+//            assertEquals(true, false);
+//        }
+//    }
 
 
 }
