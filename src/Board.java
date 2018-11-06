1336dbbf3353c750512efccc4a7581e63d917364
Krista Gurney <gurneykri@gmail.com>
2018-11-05 20:09:27 -0700

    working on testing

--- src/Board.java
+++ src/Board.java
@@ -7,7 +7,7 @@ public class Board {
     int boardSize;
     char possibleValues[];
     char actualValues[][];
-   // Cell[][] cellArray = new Cell[][];
+    // Cell[][] cellArray = new Cell[][];
 
 
     public int getBoardSize() {
@@ -15,10 +15,10 @@ public class Board {
     }
 
     public void setBoardSize(int boardSize) throws InvalidBoardException {
-        System.out.println("Board size => "+ boardSize);
-        if(boardSize != 4 && boardSize !=  9 && boardSize !=  16 && boardSize !=  25) {
+        System.out.println("Board size => " + boardSize);
+        if (boardSize != 4 && boardSize != 9 && boardSize != 16 && boardSize != 25) {
             throw new InvalidBoardException("Invalid Board Size");
-        }else{
+        } else {
             this.boardSize = boardSize;
         }
     }
@@ -38,19 +38,21 @@ public class Board {
     public void setActualValues(char[][] actualValues) {
         this.actualValues = actualValues;
     }
-    public void printBoard(){
-        for(char n: possibleValues){
-            System.out.print(n +" ");
+
+    public void printBoard() {
+        for (char n : possibleValues) {
+            System.out.print(n + " ");
         }
         System.out.println();
-        for(int row = 0; row < boardSize; row++){
-            for(int col = 0; col < boardSize; col++){
-                System.out.print(actualValues[row][col] +" ");
+        for (int row = 0; row < boardSize; row++) {
+            for (int col = 0; col < boardSize; col++) {
+                System.out.print(actualValues[row][col] + " ");
             }
             System.out.println();
         }
     }
-    public void loadBoard(String fileName)throws IOException, InvalidBoardException{
+
+    public void loadBoard(String fileName) throws IOException, InvalidBoardException {
 
         File file = new File(fileName);
 
@@ -65,169 +67,215 @@ public class Board {
 
         line = br.readLine();
         String possibleValueString[] = line.split(" ");
-        char possibleValues[]= new char[boardSize];
-        if(boardSize != possibleValueString.length){
+        char possibleValues[] = new char[boardSize];
+        if (boardSize != possibleValueString.length) {
             throw new InvalidBoardException("Possible value length does not match board size");
         }
-        int counter;
-        for(String n: possibleValueString) {
-            counter = 0;
-            char characterArray[] =  n.toCharArray();
-            if(characterArray.length == 1){
-                possibleValues[counter++]= characterArray[0];
-            }else{
+        int counter = 0;
+        for (String n : possibleValueString) {
+            char characterArray[] = n.toCharArray();
+            if (characterArray.length == 1) {
+                possibleValues[counter++] = characterArray[0];
+            } else {
                 throw new InvalidBoardException("Invalid value detected");
             }
         }
         board.setPossibleValues(possibleValues);
 
 
-        char actualValue[][]= new char[boardSize][boardSize];
-        for(int row = 0; row < boardSize; row++){//get the actual board
+        char actualValue[][] = new char[boardSize][boardSize];
+        for (int row = 0; row < boardSize; row++) {//get the actual board
             counter = 0;
             line = br.readLine();
             String boardValues[] = line.split(" ");
-            for(String n: boardValues){
+            for (String n : boardValues) {
                 //char valueArray[]= n.toCharArray();
-                char characterArray[] =  n.toCharArray();
-                if(characterArray.length == 1){
+                char characterArray[] = n.toCharArray();
+                if (characterArray.length == 1) {
                     actualValue[row][counter++] = characterArray[0];
-                }else{
+                } else {
                     throw new Error("Invalid value detected");
                 }
             }
         }
         board.setActualValues(actualValue);
         board.printBoard();
-       // board.checkRow('4', 2);
-       // board.checkCol('3', 2);
-        board.checkBlock('3',0, 1);
+        // board.checkRow('4', 2);
+        // board.checkCol('3', 2);
+        board.checkBlock('3', 0, 1);
+        board.validateBoardValues();
 
 
     }
-    public boolean checkRow(char value, int row){
-        if(row <= boardSize){
-            for(int col = 0; col < boardSize; col++){
-                if(value == actualValues[row][col]){
-                    System.out.println("Value " + value +" is in row " + row);
+
+    public boolean checkRow(char value, int row) {
+        if (row <= boardSize) {
+            for (int col = 0; col < boardSize; col++) {
+                if (value == actualValues[row][col]) {
+                    System.out.println("Value " + value + " is in row " + row);
                     return true;
                 }
             }
             System.out.println();
         }
-        System.out.println("Value " + value +" was not in row " + row);
+        System.out.println("Value " + value + " was not in row " + row);
         return false;
     }
 
-    public boolean checkCol(char value, int col){
-        if(col <= boardSize){
-            for(int row = 0; row < boardSize; row++){
-                if(value == actualValues[row][col]){
-                    System.out.println("Value " + value +" is in col " + col);
+    public boolean checkCol(char value, int col) {
+        if (col <= boardSize) {
+            for (int row = 0; row < boardSize; row++) {
+                if (value == actualValues[row][col]) {
+                    System.out.println("Value " + value + " is in col " + col);
                     return true;
                 }
             }
             System.out.println();
         }
-        System.out.println("Value " + value +" was not in col " + col);
+        System.out.println("Value " + value + " was not in col " + col);
         return false;
     }
 
-    public boolean checkBlock(char value, int row, int col){
+    public boolean checkBlock(char value, int row, int col) {
         int rowStart = 0;
         int rowEnd = 0;
         int colStart = 0;
         int colEnd = 0;
-        if(boardSize == 4){
-            if(row < 2){
-                rowStart = 0; rowEnd = 1;
-            }else{
-                rowStart = 2; rowEnd = 3;
+        if (boardSize == 4) {
+            if (row < 2) {
+                rowStart = 0;
+                rowEnd = 1;
+            } else {
+                rowStart = 2;
+                rowEnd = 3;
             }
-            if(col < 2){
-                colStart = 0; colEnd = 1;
-            }else{
-                colStart = 2; colEnd = 3;
+            if (col < 2) {
+                colStart = 0;
+                colEnd = 1;
+            } else {
+                colStart = 2;
+                colEnd = 3;
             }
 
         }
 
-        if(boardSize == 9){
-            if(row < 3){
-                rowStart = 0; rowEnd = 2;
-            }else if(row < 6){
-                rowStart = 3; rowEnd = 5;
-            }else{
-                rowStart = 6; rowEnd = 8;
+        if (boardSize == 9) {
+            if (row < 3) {
+                rowStart = 0;
+                rowEnd = 2;
+            } else if (row < 6) {
+                rowStart = 3;
+                rowEnd = 5;
+            } else {
+                rowStart = 6;
+                rowEnd = 8;
             }
 
-            if(col < 3){
-                colStart = 0; colEnd = 2;
-            }else if(col < 6){
-                colStart = 3; colEnd = 5;
-            }else{
-                colStart = 6; colEnd = 8;
+            if (col < 3) {
+                colStart = 0;
+                colEnd = 2;
+            } else if (col < 6) {
+                colStart = 3;
+                colEnd = 5;
+            } else {
+                colStart = 6;
+                colEnd = 8;
             }
         }
 
-        if(boardSize == 16){
-            if(row < 4){
-                rowStart = 0; rowEnd = 3;
-            }else if(row < 8){
-                rowStart = 4; rowEnd = 7;
-            }else if(row < 12){
-                rowStart = 8; rowEnd = 11;
-            }else{
-                rowStart = 12; rowEnd = 15;
+        if (boardSize == 16) {
+            if (row < 4) {
+                rowStart = 0;
+                rowEnd = 3;
+            } else if (row < 8) {
+                rowStart = 4;
+                rowEnd = 7;
+            } else if (row < 12) {
+                rowStart = 8;
+                rowEnd = 11;
+            } else {
+                rowStart = 12;
+                rowEnd = 15;
             }
-            if(col < 4){
-                colStart = 0; colEnd = 3;
-            }else if(row < 8){
-                colStart = 4; colEnd = 7;
-            }else if(row < 12){
-                colStart = 8; colEnd = 11;
-            }else{
-                colStart = 12; colEnd = 15;
+            if (col < 4) {
+                colStart = 0;
+                colEnd = 3;
+            } else if (row < 8) {
+                colStart = 4;
+                colEnd = 7;
+            } else if (row < 12) {
+                colStart = 8;
+                colEnd = 11;
+            } else {
+                colStart = 12;
+                colEnd = 15;
             }
         }
 
-        if(boardSize == 25){
-            if(row < 5){
-                rowStart = 0; rowEnd = 4;
-            }else if(row < 10){
-                rowStart = 5; rowEnd = 9;
-            }else if(row < 15){
-                rowStart = 10; rowEnd = 14;
-            }else if(row < 20){
-                rowStart = 15; rowEnd = 19;
-            }else{
-                rowStart = 20; rowEnd = 24;
+        if (boardSize == 25) {
+            if (row < 5) {
+                rowStart = 0;
+                rowEnd = 4;
+            } else if (row < 10) {
+                rowStart = 5;
+                rowEnd = 9;
+            } else if (row < 15) {
+                rowStart = 10;
+                rowEnd = 14;
+            } else if (row < 20) {
+                rowStart = 15;
+                rowEnd = 19;
+            } else {
+                rowStart = 20;
+                rowEnd = 24;
             }
 
-            if(col < 5){
-                colStart = 0; colEnd = 4;
-            }else if(row < 10){
-                colStart = 5; colEnd = 9;
-            }else if(row < 15){
-                colStart = 10; colEnd = 14;
-            }else if(row < 20){
-                colStart = 15; colEnd = 19;
-            }else{
-                colStart = 20; colEnd = 24;
+            if (col < 5) {
+                colStart = 0;
+                colEnd = 4;
+            } else if (row < 10) {
+                colStart = 5;
+                colEnd = 9;
+            } else if (row < 15) {
+                colStart = 10;
+                colEnd = 14;
+            } else if (row < 20) {
+                colStart = 15;
+                colEnd = 19;
+            } else {
+                colStart = 20;
+                colEnd = 24;
             }
         }
-        for(int rowCounter = rowStart; rowCounter <= rowEnd; rowCounter++){
-            for(int colCounter = colStart; colCounter <= colEnd; colCounter++){
-                if(value == actualValues[rowCounter][colCounter]){
-                    System.out.println("Start Row " + rowStart + " end row " +rowEnd + " start column " + colStart + " end column "+ colEnd);
-                    System.out.println("Value " + value + " in [" + rowCounter +"][" + colCounter + "]");
+        for (int rowCounter = rowStart; rowCounter <= rowEnd; rowCounter++) {
+            for (int colCounter = colStart; colCounter <= colEnd; colCounter++) {
+                if (value == actualValues[rowCounter][colCounter]) {
+                    System.out.println("Start Row " + rowStart + " end row " + rowEnd + " start column " + colStart + " end column " + colEnd);
+                    System.out.println("Value " + value + " in [" + rowCounter + "][" + colCounter + "]");
                     return true;
                 }
             }
 
         }
         //System.out.println("Start Row " + rowStart + " end row " +rowEnd + " start column " + colStart + " end column "+ colEnd);
-        System.out.println("Value " + value + " is not in [" + row +"][" + col + "]");
+        System.out.println("Value " + value + " is not in [" + row + "][" + col + "]");
         return false;
+    }
+
+    void validateBoardValues() throws InvalidBoardException{//makes sure the board values are one of the possible values
+        for(int i = 0; i < boardSize; i++) {
+            for(int j = 0; j < boardSize;j++ ){
+                boolean isValid = false;
+                for(int k = 0; k < boardSize; k++) {
+                    if(actualValues[i][j] == possibleValues[k] || actualValues[i][j] == '-' ){
+                        isValid = true;
+                        break;
+                    }
+                }
+                if(!isValid) {
+                    throw new InvalidBoardException("Invalid character in the board in row" + i);
+                }
+            }
+        }
     }
 }
--- test/BoardTest.java
+++ test/BoardTest.java
@@ -8,11 +8,101 @@ import static org.junit.Assert.*;
 
 public class BoardTest{
 
+//    @Test
+//    public void loadInvalidBoard(){
+//        Board board = new Board();
+//        try {
+//            board.loadBoard("C:\\Users\\Krista Gurney\\Documents\\cs5700\\sudokuSolver\\test\\invalidBoardSize.txt");
+//            assertEquals(true, false);
+//        }catch (IOException e){
+//            e.printStackTrace();
+//        }catch (InvalidBoardException e){
+//            assertEquals(true, true);
+//        }
+//    }
+//
+//    @Test
+//    public void loadInvalidPossibleValues(){
+//        Board board = new Board();
+//        try {
+//            board.loadBoard("C:\\Users\\Krista Gurney\\Documents\\cs5700\\sudokuSolver\\test\\invalidPossibleValues.txt");
+//            assertEquals(true, false);
+//        }catch (IOException e){
+//            e.printStackTrace();
+//        }catch (InvalidBoardException e){
+//            assertEquals(true, true);
+//        }
+//    }
+//
+//    @Test
+//    public void tooManyPossibleValues(){
+//        Board board = new Board();
+//        try {
+//            board.loadBoard("C:\\Users\\Krista Gurney\\Documents\\cs5700\\sudokuSolver\\test\\tooManyPossibleValues.txt");
+//            assertEquals(true, false);
+//        }catch (IOException e){
+//            e.printStackTrace();
+//        }catch (InvalidBoardException e){
+//            assertEquals(true, true);
+//        }
+//    }
+//
+//    @Test
+//    public void invalidValue(){
+//        Board board = new Board();
+//        try {
+//            board.loadBoard("C:\\Users\\Krista Gurney\\Documents\\cs5700\\sudokuSolver\\test\\invalidValue.txt");
+//            assertEquals(true, false);
+//        }catch (IOException e){
+//            e.printStackTrace();
+//        }catch (InvalidBoardException e){
+//            assertEquals(true, true);
+//        }
+//    }
+
     @Test
-    public void loadInvalidBoard(){
+    public void validateCheckRow(){
         Board board = new Board();
         try {
-            board.loadBoard("C:\\Users\\Krista Gurney\\Documents\\cs5700\\sudokuSolver\\test\\invalidBoardSize.txt");
+            board.loadBoard("C:\\Users\\Krista Gurney\\Documents\\cs5700\\sudokuSolver\\SamplePuzzles\\Puzzle-4x4-0001.txt");
+            board.checkRow('2', 0);
+            assertEquals(true, false);
+        }catch (IOException e){
+            e.printStackTrace();
+        }catch (InvalidBoardException e){
+            assertEquals(true, true);
+        }
+    }
+    @Test
+    public void validateCheckBlock(){
+        Board board = new Board();
+        try {
+            board.loadBoard("C:\\Users\\Krista Gurney\\Documents\\cs5700\\sudokuSolver\\SamplePuzzles\\Puzzle-4x4-0001.txt");
+            /*
+                2 - 3 1
+                1 3 - 4
+                3 1 4 -
+                - 2 1 3*/
+            board.checkBlock('2',0,0);
+            board.checkBlock('-', 0, 1 );
+            board.checkBlock('3', 0, 2 );
+            board.checkBlock('1', 0, 3 );
+
+            board.checkBlock('1', 1, 0 );
+            board.checkBlock('3', 1, 1 );
+            board.checkBlock('-', 1, 2 );
+            board.checkBlock('4', 1, 3 );
+
+            board.checkBlock('3', 2, 0 );
+            board.checkBlock('1', 2, 1 );
+            board.checkBlock('4', 2, 2 );
+            board.checkBlock('-', 2, 3 );
+
+            board.checkBlock('-', 3, 0 );
+            board.checkBlock('2', 3, 1 );
+            board.checkBlock('1', 3, 2 );
+            board.checkBlock('3', 3, 3 );
+
             assertEquals(true, false);
         }catch (IOException e){
             e.printStackTrace();
@@ -21,29 +111,5 @@ public class BoardTest{
         }
     }
 
-    @Test
-    public void loadInvalidPossibleValues(){
-        Board board = new Board();
-        try {
-            board.loadBoard("C:\\Users\\Krista Gurney\\Documents\\cs5700\\sudokuSolver\\test\\invalidPossibleValues.txt");
-            assertEquals(true, false);
-        }catch (IOException e){
-            e.printStackTrace();
-        }catch (InvalidBoardException e){
-            assertEquals(true, true);
-        }
-    }
 
-    @Test
-    public void tooManyPossibleValues(){
-        Board board = new Board();
-        try {
-            board.loadBoard("C:\\Users\\Krista Gurney\\Documents\\cs5700\\sudokuSolver\\test\\tooManyPossibleValues.txt");
-            assertEquals(true, false);
-        }catch (IOException e){
-            e.printStackTrace();
-        }catch (InvalidBoardException e){
-            assertEquals(true, true);
-        }
-    }
 }
